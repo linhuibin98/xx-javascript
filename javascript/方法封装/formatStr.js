7633e5d355c4250d406dff2c82cde5860f69e402
lhb1032352370 <1032352370@qq.com>
2019-09-16 12:49:39 +0800

    'formatstr'

--- javascript/方法封装/formatStr.js
+++ javascript/方法封装/formatStr.js
@@ -17,8 +17,14 @@ console.log(formatStr(template, '美好', '暴躁'));   // 世界如此美好，
 */
 
 // 进阶版
-function formatStr(str, obj) {
-
+function formatStr(str, data) {  // data 为数据对象
+    return str.replace(/\${([\s\S]+?)}/g, function (item) {
+        item = item.replace(/[ ]/g, '');
+        let n = item.slice(2, item.length - 1);
+        return typeof data[n] !== 'undefined' ? data[n] : item;
+    })
 }
+let template = "世界如此${i}，你却如此${b}！";
 
+console.log(formatStr(template, {i: '美好', b: '暴躁'}));
 module.exports = formatStr;
